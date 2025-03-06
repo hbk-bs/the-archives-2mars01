8ee60956addc4bdac08365d55b8de0eeedc213fd
Maggie <mar.schaefer@hbk-bs.de>
2025-03-06 16:42:46 +0100

    pleaseeee

--- perspectives/birdseye/index.js
+++ perspectives/birdseye/index.js
@@ -4,14 +4,9 @@ const s = (n) => width * n;
 
 function setup() {
 	createCanvas(500, 250);
-	let c1= color(255, 81,61);
-	let c2= color(168,17,0);
-	let c3= color(255);
-	let c4= color(214,100,82);
-	let c5= color(111,255,10);
-	background(c1);
+	background("tomato");
 	noStroke();
-	fill(c2);
+	fill("orangered");
 
 	beginShape();
 	vertex(x(0.5), 0);
@@ -20,7 +15,7 @@ function setup() {
 	vertex(x(0.5), y(1));
 	endShape();
 
-	fill(c3);
+	fill("white");
 	rect(x(0.5), 0, y(0.002), x(1));
 	rect(0, y(1), x(1), y(0.002));
 	rect(0, y(0.2), x(1), y(0.002));
@@ -29,7 +24,7 @@ function setup() {
 	rect(x(0.75), y(0.2), y(0.002), 300);
 	rect(x(0.25), y(0.5), x(0.5), y(0.002));
 
-	fill(c4);
+	fill("salmon");
 	rect(x(0.5) + 3, y(0.2), 37, y(0.002));
 	rect(x(0.5) + 3, y(0.5), 37, y(0.002));
 	rect(x(0.5) + 3, y(0.8), 37, y(0.002));
@@ -37,13 +32,15 @@ function setup() {
 	for (let i = 0; i < 5; i++) {
 		const x1 = random(1);
 		const y1 = random(1);
-		fill(c2);
+		fill("crimson");
 		ellipse(x(x1), y(y1), s(0.01));
 
-		fill(c5);
+		fill("lawngreen");
 		ellipse(x(x1 - 0.002), y(y1 - 0.002), s(0.01));
 	}
+	
 }
+
 function keyPressed() {
 	if (key === "s") {
 		const name = prompt("Enter name", `out-${Date.now()}.png`);
